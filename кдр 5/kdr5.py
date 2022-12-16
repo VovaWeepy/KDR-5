829cda9d33940952321a4af315b68fccbdee1b19
VovaWeepy <117993619+VovaWeepy@users.noreply.github.com>
2022-12-16 09:32:03 +0300

    Add files via upload

---
+++ кдр 5/kdr5.py
@@ -0,0 +1,14 @@
+def f(n):
+    for i in range(0, len(n) - 1):
+        minn = i
+        for j in range(i + 1, len(a)):
+            if n[j] < n[minn]:
+                minn = j
+        n[i], n[minn] = n[minn], n[i]
+a=[int(x) for x in open('rt.txt')]       
+f(a)
+import json
+with open('file.txt','w')as f:
+    json.dump(a,f)
+print('сортированный список: ', end='')
+print(a)
